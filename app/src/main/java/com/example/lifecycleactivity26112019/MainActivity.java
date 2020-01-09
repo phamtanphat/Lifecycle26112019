aee7330f5bdbf7b4a1f4b3941c8d05b0c603e4de
phatdroid94 <phatdroid94@gmail.com>
2020-01-09 19:01:28 +0700

    init

---
+++ app/src/main/java/com/example/lifecycleactivity26112019/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.lifecycleactivity26112019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("BBB","OnCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("BBB","onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("BBB","onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("BBB","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("BBB","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("BBB","onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("BBB","onDestroy");
+    }
+}
